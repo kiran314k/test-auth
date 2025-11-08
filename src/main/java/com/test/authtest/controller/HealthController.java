8ff706a2da99c1a12fe652e1277ae337502598e3
kiran <kiran314k@gmail.com>
2025-11-08 09:26:44 +0530

    testing

--- src/main/java/com/test/authtest/controller/HealthController.java
+++ src/main/java/com/test/authtest/controller/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-    @GetMapping("/health")
+    @GetMapping({"/health", "/actuator/health", "/health/readiness", "/health/liveness"})
     public String healthCheck() {
         return "Application is running!";
     }
